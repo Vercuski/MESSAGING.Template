fafc4c517e9c3ae3a6c74ee825797c0b66ee42cf
Scott Vercuski <scott.vercuski@gmail.com>
2024-04-21 17:41:38 -0400

    Removing Messages.Tests code

--- source/MESSAGING.Tests/ArchitectureTests/AssemblyReferences.cs
+++
@@ -1,9 +0,0 @@
-﻿using MESSAGING.Messages.Commands;
-using System.Reflection;
-
-namespace MESSAGING.Tests.ArchitectureTests;
-
-internal static class AssemblyReferences
-{
-    internal static readonly Assembly EventsAssembly = typeof(SampleDocument).Assembly;
-}
